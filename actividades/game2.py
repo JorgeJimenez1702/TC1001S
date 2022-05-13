62ee0d1ba9ce7590df6e1ef97ef09b06459b60e0
JorgeJimenez1702 <83251551+JorgeJimenez1702@users.noreply.github.com>
2022-05-12 19:22:14 -0700

    Juego terminado

--- actividades/game2.py
+++ actividades/game2.py
@@ -3,10 +3,14 @@
 from turtle import *
 from random import randrange
 from freegames import square, vector
+import random
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+ran = random.randint(0, 4)
+ran2 = random.randint(0, 4)
+a = ["black", "blue", "yellow", "green", "purple"]
 
 def change(x, y):
     "Change snake direction."
@@ -39,9 +43,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, a[ran2])
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, a[ran])
     update()
     ontimer(move, 100)
 
